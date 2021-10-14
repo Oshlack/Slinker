c5e51c76caec610b4c51e48cd300308c5628b453
Breon Schmidt <breon.schmidt@gmail.com>
2021-10-14 13:00:02 +1100

    Include python tools in semi-automated install

--- dependencies_linux64.sh
+++ dependencies_linux64.sh
@@ -84,7 +84,7 @@ fi
 echo "Checking that all required tools were installed:"
 Final_message="All commands installed successfully!"
 for c in $commands ; do
-    c_path=`which $PWD/bin/$c 2>/dev/null`
+    c_path=`which $PWD/tools/bin/$c 2>/dev/null`
     if [ -z $c_path ] ; then
     echo -n "WARNING: $c could not be found!!!! "
     echo "You will need to download and install $c manually, then add its path to workflows/tools.groovy"
